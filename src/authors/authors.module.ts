29eca87d249e5f8de97a2278cffb7ceb2c28863b
Дмитрий Швалев <m2109465@edu.misis.ru>
2023-03-22 18:58:38 +0300

    commit

--- src/authors/authors.module.ts
+++ src/authors/authors.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthorsController } from './authors.controller';
+import { AuthorsService } from './authors.service';
 import { Author } from './entities/authors.entity';
 
 @Module({
   imports: [Author],
-  controllers: [],
-  providers: [],
+  controllers: [AuthorsController],
+  providers: [AuthorsService],
 })
 export class AuthorsModule {}
